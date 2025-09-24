f6fba9e0e83d0d91a905d534da7d6ea44b11ecf0
Thee <mrtheeebanda.github@gmail.com>
2025-09-25 04:38:41 +0700

    feat(frontend): import Footer in main layout and position at bottom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -15,6 +16,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
+      <Home />
+      <Footer /> 
     </BrowserRouter>
   </React.StrictMode>
 );
