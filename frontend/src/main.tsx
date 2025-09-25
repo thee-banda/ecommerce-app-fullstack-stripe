70803661e200b4061150a8b9db52b88b65cef362
Thee <mrtheeebanda.github@gmail.com>
2025-09-25 23:41:10 +0700

    feat: integrate LocaleProvider in main.tsx
    - wrap BrowserRouter with LocaleProvider for global locale support
    - adjust imports to include LocaleContext
    - ensure Shop page route is registered

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
+import { LocaleProvider } from "./context/LocaleContext"
 import Cart from "./pages/Cart"
 import Checkout from "./pages/Checkout"
 import Home from "./pages/Home"
@@ -11,17 +12,19 @@ import Signup from "./pages/Signup"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="shop" element={<Shop />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="checkout" element={<Checkout />} />
-          <Route path="auth/login" element={<Login />} />
-          <Route path="auth/signup" element={<Signup />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <LocaleProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="auth/login" element={<Login />} />
+            <Route path="auth/signup" element={<Signup />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </LocaleProvider>
   </React.StrictMode>
 )
